a94ac931379cb9ca33c7f3d10bb3b8932262e5af
Peter Kröner <peter@peterkroener.de>
2013-06-05 21:49:17 +0200

    New, working photo library + tests

--- test/photo.js/photoTest.js
+++ test/photo.js/photoTest.js
@@ -6,15 +6,17 @@ define(['lib/photo'], function(photo){
     equal(typeof photo.isRecording, 'boolean', 'photo.isRecording ist ein Boolean');
     equal(typeof photo.init, 'function', 'photo.init ist eine Funktion');
     equal(photo.init.length, 1, 'photo.init akzeptiert einen Parameter');
-    equal(typeof photo.drawToCanvas, 'function', 'photo.drawToCanvas ist eine Funktion');
-    equal(photo.drawToCanvas.length, 0, 'photo.drawToCanvas akzeptiert null Parameter');
     equal(typeof photo.startRecording, 'function', 'photo.startRecording ist eine Funktion');
     equal(photo.startRecording.length, 1, 'photo.startRecording akzeptiert einen Parameter');
     equal(typeof photo.stopRecording, 'function', 'photo.stopRecording ist eine Funktion');
     equal(photo.stopRecording.length, 0, 'photo.stopRecording akzeptiert null Parameter');
+    equal(typeof photo.getVideo, 'function', 'photo.getVideo ist eine Funktion');
+    equal(photo.getVideo.length, 0, 'photo.getVideo akzeptiert null Parameter');
+    equal(typeof photo.getCanvas, 'function', 'photo.getCanvas ist eine Funktion');
+    equal(photo.getCanvas.length, 0, 'photo.getCanvas akzeptiert null Parameter');
   });
 
-  test('supportsRecording', function(){
+  test('photo.supportsRecording', function(){
     var api = navigator.getUserMedia ||
               navigator.webkitGetUserMedia ||
               navigator.mozGetUserMedia ||
@@ -24,14 +26,32 @@ define(['lib/photo'], function(photo){
     equal(photo.supportsRecording, true, 'Browserunterstützung für Tests vorhanden');
   });
 
-  test('drawToCanvas', function(){
-    console.log(photo)
+  test('Init und Getter', function(){
+    photo.init('canvas');
+    equal(photo.getCanvas().toString(), '[object HTMLCanvasElement]', 'Canvas wird initialisiert und bereitgestellt');
+    equal(photo.getVideo().toString(), '[object HTMLVideoElement]', 'Video wird initialisiert und bereitgestellt');
   });
 
-  asyncTest('startRecording', function(){
+  test('Aufnahme und Aufnahmestopp', function(){
+    stop(2);
+    photo.init('canvas');
 
-    test('stopRecording', function(){
+    photo.startRecording(function(){
+      ok(true, 'Callback von photo.startRecording() wird ausgeführt');
+      ok(photo.isRecording, 'photo.isRecording ist nach photo.startRecording() true');
+      equal(photo.getVideo().paused, false, 'Video wird nach photo.startRecording() abgespielt');
+      equal(photo.getVideo().videoWidth, photo.getCanvas().width, 'Canvas-Breite === Stream-Breite');
+      equal(photo.getVideo().videoHeight, photo.getCanvas().height, 'Canvas-Höhe === Stream-Höhe');
+      start();
+      setTimeout(function(){
+        photo.stopRecording();
+        ok(!photo.isRecording, 'photo.isRecording ist nach photo.stopRecording() false');
+        equal(photo.getVideo().paused, true, 'Video ist nach photo.stopRecording() gestoppt');
+        start();
+      }, 2000);
     });
+
+
   });
 
 });
