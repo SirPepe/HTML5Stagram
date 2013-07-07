ab1711bd1fa3b587db2d0013fc6aea90926760d4
Peter Kröner <peter@peterkroener.de>
2013-07-08 00:00:35 +0200

    Fehlerhaft-suboptimalen Drop-Test gefixt

--- test/drop.js/dropTest.js
+++ test/drop.js/dropTest.js
@@ -1,4 +1,4 @@
-define(['lib/drop'], function(drop){
+define(['lib/drop', 'lib/vendor/jquery'], function(drop){
 
   var element = document.getElementById('Droptest');
 
@@ -10,13 +10,14 @@ define(['lib/drop'], function(drop){
   asyncTest('Modul-Funktionalität', function(){
     drop('#Droptest', function(evt){
       ok(true, 'Callback feuert');
+      equal(this, $('#Droptest')[0], '"this" im drop-Callback ist das Drop-Ziel');
       ok($(this).hasClass('active'), 'active-Klasse wird angewendet');
       equal(typeof evt, 'object', 'drop-Callback übergibt Event-Objekt');
       stop();
       setTimeout(function(){
         ok(!$(this).hasClass('active'), 'active-Klasse wird wieder entfernt');
         start();
-      }, 500);
+      }.bind(this), 500);
       start();
     });
   });
